0f8c38fbf7eedc8e438fa461ac972f1da297fd88
Rim Choi <sandrochia@naver.com>
2021-03-07 18:55:37 +0900

    1.10 JSX 내 자바스크립트 객체 처리

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends Component {
   render() {
-    
-    const hello = 'Hi, React 도움닫기 ..!';
-    
+        
     return (
       
       <div className="App"> 
-        <h2>{hello}</h2>
+        {list.map(function (item) {
+          return (
+            /**
+             * 배열의 인덱스 값은 고정값이 아니기 때문에 사용하지 말아야 합니다. 
+             * 배열의 순서가 변경되면, 리액트가 각 항목을 식별할 수 없기 때문입니다.
+             */
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          );
+        })}
       </div>
     );
   }
