36a28f8a52cdce628fb994493046d34e60b619a2
Rim Choi <sandrochia@naver.com>
2021-03-07 18:18:33 +0900

    1.7 ES6 const · let

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 
 class App extends Component {
   render() {
-    var hello = 'Hi, React 도움닫기 ..!';
-    // 속성은 카멜 표기법을 따른다
+    /**
+     * let - 변수 재할당 가능.
+     * const - 변수 재할당 X, 변수가 배열이나 객체 타입일 경우 변경 가능. 
+     * 불변 데이터 구조(immutable data structures)
+     * 한 번 데이터 구조가 정의된 이후, 다시 변경할 수 없습니다.
+     */
+    const hello = 'Hi, React 도움닫기 ..!';
+    
     return (
       
       <div className="App"> 
