7f23e69fb529bdd3c93edd0f1e35328d42a89d19
Rim Choi <sandrochia@naver.com>
2021-03-07 19:12:25 +0900

    2.1 컴포넌트 내부 상태 관리

--- src/App.js
+++ src/App.js
@@ -19,23 +19,36 @@ const list = [
     objectID: 1,
   },
 ];
-
+/**
+ * 2.1 Internal component state
+ * 컴포넌트 내부 상태(Internal component state)는 컴포넌트에 저장된 상태(state)를 말하며, 
+ * 다른 말로는 로컬 상태(local state)라고 합니다. 
+ * 
+ * 컴포넌트 상태는 프로퍼티로 그 값을 수정, 삭제, 저장합니다.
+ */
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: list,
+    }
+  }
+
   render() {
         
     return (
       
       <div className="App"> 
-        {list.map(item => 
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </div>
-          )}
+        {this.state.list.map(item => 
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>  
+        )}
       </div>
     );
   }
