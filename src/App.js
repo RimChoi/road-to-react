225d74da9b38fd32c96351981aedcdfe13bf70e5
Rim Choi <sandrochia@naver.com>
2021-03-07 18:59:47 +0900

    1.11 ES6 arrow function

--- src/App.js
+++ src/App.js
@@ -26,12 +26,7 @@ class App extends Component {
     return (
       
       <div className="App"> 
-        {list.map(function (item) {
-          return (
-            /**
-             * 배열의 인덱스 값은 고정값이 아니기 때문에 사용하지 말아야 합니다. 
-             * 배열의 순서가 변경되면, 리액트가 각 항목을 식별할 수 없기 때문입니다.
-             */
+        {list.map(item => 
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
@@ -40,8 +35,7 @@ class App extends Component {
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
             </div>
-          );
-        })}
+          )}
       </div>
     );
   }
